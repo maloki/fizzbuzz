a9042b8bf73161698adcfe8746d921bc3cf76e0e
maloki <malokiusa@gmail.com>
2018-06-04 18:54:11 +0200

    s

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const path = require('path');
+const port = 3000
 
 app.get("/", function (req, res) {
  res.sendFile(path.join(__dirname, "../", "main.html"));
 });
 
-app.listen(3000, () => console.log("working"))
+app.listen(process.env.PORT || port, () => console.log("working"))
